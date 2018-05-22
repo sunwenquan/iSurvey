c8e9d20db0e0f1ab1c2157496c6c1c9a63fd7f7d
gavinsun88 <sunwenquan@sina.cn>
2018-05-23 07:26:31 +0800

    model设计

---
+++ test/time_oper.py
@@ -0,0 +1,16 @@
+import time,datetime
+import pytz
+from tzlocal import get_localzone # $ pip install tzlocal
+# 1. 如何设置时区
+# set timezone
+tz = pytz.timezone('Asia/Shanghai')
+# get local timezone  
+local_tz = get_localzone() 
+# get all_timezones 
+# all_timezones = pytz.all_timezones 
+# print(all_timezones)
+print(tz)
+print(local_tz)
+# 2. 获取时间
+print(datetime.datetime.now())
+print(datetime.datetime.utcnow())
