ec30cb2bf742ef09a60950bc9b801a666b3da685
Your Name <you@example.com>
2018-05-23 14:27:42 +0800

    创建manage.py,用来管理app和db

--- backend/manage.py
+++ backend/manage.py
@@ -1,1 +1,26 @@
 from surveyapi import api
+from flask_script import Manager
+from flask_migrate import MigrateCommand,Migrate
+from surveyapi.app import create_app
+from surveyapi.models import db,Survey,Question,Choice,User
+# pip install flask-script
+# pip install flask-migrate
+
+app = create_app()
+# 创建migrate实例
+migrate = Migrate(app,db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+@manager.shell
+def shell_ctx():
+    return dict(app=app,
+        db=db,
+        Surver=Survey,
+        Question=Question,
+        User=User,
+        Choice=Choice
+    )
+
+if __name__ == '__main__':
+    manager.run()
